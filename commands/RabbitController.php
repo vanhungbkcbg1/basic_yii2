15cd52f28bdb977e704f2e214520591e9d775b37
hungnv <vanhungbkcbg1@gmail.com>
2020-05-05 17:18:02 +0700

    rabbit pubsub

--- commands/RabbitController.php
+++ commands/RabbitController.php
@@ -8,6 +8,7 @@
 namespace app\commands;
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use yii\helpers\Console;
@@ -41,6 +42,84 @@ class RabbitController extends Controller
         };
 
         $channel->basic_consume('demo', '', false, true, false, false, $callback);
+
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
+    }
+
+    public function actionQueue($message = 'hello world')
+    {
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('demo', false, true, false, false);
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+            sleep(substr_count($msg->body, '.'));
+            echo " [x] Done\n";
+
+        };
+
+        $channel->basic_consume('demo', '', false, true, false, false, $callback);
+
+        while ($channel->is_consuming()) {
+            $channel->wait();
+        }
+
+        $channel->close();
+        $connection->close();
+    }
+
+    public function actionPush($message){
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+        $channel = $connection->channel();
+
+        $channel->queue_declare('demo', false, true, false, false);
+
+        $msg = new AMQPMessage($message);
+        $channel->basic_publish($msg, '', 'demo');
+
+
+        $channel->close();
+        $connection->close();
+    }
+
+    public function actionPublish($message="Hello Publish"){
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+        $channel = $connection->channel();
+
+//        $channel->queue_declare('demo', false, true, false, false);
+
+        $msg = new AMQPMessage($message);
+        $channel->basic_publish($msg, 'demo_fanout');
+
+
+        $channel->close();
+        $connection->close();
+        echo "Done";
+    }
+
+    public function actionSubscribe(){
+        $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+        $channel = $connection->channel();
+
+
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+
+        };
+
+        // I am
+        $channel->basic_consume('fanout_1', '', false, true, false, false, $callback);
 
         while ($channel->is_consuming()) {
             $channel->wait();
